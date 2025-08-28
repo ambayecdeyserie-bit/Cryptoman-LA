17b3621cece0e79e98ffe0199dc9ef466c495fb8
ambayecdeyserie-bit <ambayecdeyserie@gmail.com>
2025-08-28 08:25:45 +0800

    feat: add Hardhat deployment essentials for blockchain deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
 require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.21",
   networks: {
-    skale: {
-      url: process.env.SKALE_RPC,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+    goerli: {
+      url: process.env.GOERLI_RPC_URL || "https://rpc.ankr.com/eth_goerli",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : []
     }
   }
 };
